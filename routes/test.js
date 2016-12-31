5115cc4b8e4351d1b75f4bc5505b4d6ebf11e0b8
eccolala <764884989@qq.com>
2016-12-31 15:47:50 +0800

    Initial commit

---
+++ routes/test.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: "1123",
+    port : 3306, //port mysql
+    database: 'Test'
+});
+
+connection.connect();
+
+
+/* GET test page. */
+router.get('/test', function(request, response, next) {
+    connection.query('SELECT * FROM login', function(err, rows, fields) {
+        if (err) {
+            console.log('Encountered an error:', err.message);
+            return response.send(500, err.message);
+        }
+        data = ({'data' : rows});
+        console.log(data);
+
+        response.json(data);
+    });
+
+
+});
+
+module.exports = router;
